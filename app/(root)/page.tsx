92fa73ed0ebf6d876e2e4172a22329c6cb913269
shubham <shubham@quickads.ai>
2025-02-26 13:16:34 +0530

    Add Product Data and Update Home Page to Display Products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,51 +1,10 @@
 import Link from "next/link";
 
-import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
+import { productsData } from "@/constants/productData";
 import ROUTES from "@/constants/routes";
-
-const questions = [
-  {
-    _id: "1",
-    title: "How to learn React?",
-    description: "I want to learn React, can anyone help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "JavaScript" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      image:
-        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
-    },
-    upvotes: 10,
-    answers: 5,
-    views: 100,
-    createdAt: new Date(),
-  },
-  {
-    _id: "2",
-    title: "How to learn JavaScript?",
-    description: "I want to learn JavaScript, can anyone help me?",
-    tags: [
-      { _id: "1", name: "JavaScript" },
-      { _id: "2", name: "JavaScript" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      image:
-        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
-    },
-    upvotes: 10,
-    answers: 5,
-    views: 100,
-    createdAt: new Date("2021-09-01"),
-  },
-];
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
@@ -53,16 +12,20 @@ interface SearchParams {
 
 const Home = async ({ searchParams }: SearchParams) => {
   const { query = "", filter = "" } = await searchParams;
+  
+  const filteredProducts = Object.values(productsData).flatMap(category => 
+    category.products.filter((product) => {
+      const matchesQuery = product.productName
+        .toLowerCase()
+        .includes(query.toLowerCase());
+      
+      const matchesFilter = filter
+        ? product.category.toLowerCase() === filter.toLowerCase()
+        : true;
 
-  const filteredQuestions = questions.filter((question) => {
-    const matchesQuery = question.title
-      .toLowerCase()
-      .includes(query.toLowerCase());
-    const matchesFilter = filter
-      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
-      : true;
-    return matchesQuery && matchesFilter;
-  });
+      return matchesQuery && matchesFilter;
+    })
+  );
 
   return (
     <>
@@ -86,9 +49,13 @@ const Home = async ({ searchParams }: SearchParams) => {
       </section>
       <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
-        {filteredQuestions.map((question) => (
-          <QuestionCard key={question._id} question={question} />
-        ))}
+     
+
+        {filteredProducts.map((product) => {
+          return(
+            <h1 key={product.productName}>{product.productName}</h1>
+          )
+        })}
       </div>
     </>
   );
